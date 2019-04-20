eb4a761c0f54da6241471b383e97b4e7126a90ef
Saransh Jain <saranshj07@gmail.com>
2019-04-20 12:06:39 -0400

    Update index.ios.js

--- auth/index.ios.js
+++ auth/index.ios.js
@@ -18,14 +18,7 @@ import LoginForm from './src/components/LoginForm';
 export default class auth extends Component {
 
   componentWillMount() {
-    var config = {
-      apiKey: "AIzaSyCD0s6EG0T3wsvYDIpNrBfhWfZrB8_GFQY",
-      authDomain: "auth-3d304.firebaseapp.com",
-      databaseURL: "https://auth-3d304.firebaseio.com",
-      projectId: "auth-3d304",
-      storageBucket: "",
-      messagingSenderId: "445585099885"
-    };
+    var config = {};
     firebase.initializeApp(config);
   }
 
